f1c8ff899791bf7a0af6b13cb0ea100f7ecff1c1
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-10-16 22:05:37 +0300

    added new file

---
+++ 13/01.js
@@ -0,0 +1,43 @@
+// document.write("a");
+// document.write("a");
+// document.write("a");
+
+// for (let index = 0; index < 45; index++) {
+//   document.write("a");
+// }
+//исходные данные
+let repeatCount = 3;
+//инициализация стартового значения
+let index = 1;
+//проверка
+if (index <= repeatCount) {
+  //целевое дейстие
+  document.write("a");
+}
+//Инкрементируем индекс
+index = 2;
+// ......
+if (index <= repeatCount) {
+  //......
+  document.write("a");
+}
+index = 3;
+if (index <= repeatCount) {
+  document.write("a");
+}
+index = 4;
+if (index <= repeatCount) {
+  document.write("a");
+}
+index = 5;
+if (index <= repeatCount) {
+  document.write("a");
+}
+index = 6;
+if (index <= repeatCount) {
+  document.write("a");
+}
+index = 7;
+if (index <= repeatCount) {
+  document.write("a");
+}
---
+++ 13/02.js
@@ -0,0 +1,7 @@
+//исходные данные
+let repeatCount = 10;
+//инициализация стартового значения, условие  и увеличение счетчика на 1
+for (let index = 1; index <= repeatCount; index++) {
+  //целевое дейстие
+  document.write("a");
+}
